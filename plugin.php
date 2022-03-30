6d02def575b9468c40f933397d883477f677dd19
Marco Di Bella <mdb@marcodibella.de>
2022-03-30 10:54:37 +0200

    Umbenannt

--- plugin.php
+++ plugin.php
@@ -8,39 +8,38 @@ Version:         1.0.0
 */
 
 
-defined( 'ABSPATH' ) OR exit;
+defined( 'ABSPATH' ) or exit;
 
 
 
-/* Konstanten */
+/** Konstanten */
 
-define( 'TABLE_POOL',  'cmkk_contingent_pool' );
-define( 'TABLE_USER',  'cmkk_contingent_user' );
+define( 'TABLE_POOL', 'cmkk_contingent_pool' );
+define( 'TABLE_USER', 'cmkk_contingent_user' );
 
-define( 'OPTION_MAIL_SUBJECT',  'cmkk_plugin_mail_subject' );
-define( 'OPTION_MAIL_MESSAGE',  'cmkk_plugin_mail_message' );
+define( 'OPTION_MAIL_SUBJECT', 'cmkk_plugin_mail_subject' );
+define( 'OPTION_MAIL_MESSAGE', 'cmkk_plugin_mail_message' );
 
-define( 'STATUS_USER_ADDED',            100 );
-define( 'STATUS_NOTHING_FREE',          200 );
-define( 'STATUS_USER_FIELDS_EMPTY',     201 );
-define( 'STATUS_USER_EMAIL_MALFORMED',  202 );
-define( 'STATUS_USER_EMAIL_IN_USE',     203 );
-define( 'STATUS_CANT_STORE_USER',       204 );
+define( 'STATUS_USER_ADDED',           100 );
+define( 'STATUS_NOTHING_FREE',         200 );
+define( 'STATUS_USER_FIELDS_EMPTY',    201 );
+define( 'STATUS_USER_EMAIL_MALFORMED', 202 );
+define( 'STATUS_USER_EMAIL_IN_USE',    203 );
+define( 'STATUS_CANT_STORE_USER',      204 );
 
-//define( 'PLUGIN_URL',    plugins_url() );
 define( 'PLUGIN_PATH',   plugin_dir_path( __FILE__ ) );
 define( 'EXPORT_FOLDER', 'cmkk' );
 
-// Workaround: gegenwärtig Unterstützung nur für ein Event
-define( 'EVENT_ID', '1' );
+
+define( 'EVENT_ID', '1' ); // Workaround: gegenwärtig Unterstützung nur für ein Event
 
 
 
 /* Funktionsbibliothek einbinden */
 
-require_once( PLUGIN_PATH . 'inc/class-modified-list-table.php' );
-require_once( PLUGIN_PATH . 'inc/class-pool-list-table.php' );
-require_once( PLUGIN_PATH . 'inc/class-user-list-table.php' );
+require_once( PLUGIN_PATH . 'inc/classes/class-modified-list-table.php' );
+require_once( PLUGIN_PATH . 'inc/classes/class-pool-list-table.php' );
+require_once( PLUGIN_PATH . 'inc/classes/class-user-list-table.php' );
 require_once( PLUGIN_PATH . 'inc/shortcode-form.php' );
 require_once( PLUGIN_PATH . 'inc/core.php' );
 require_once( PLUGIN_PATH . 'inc/mainpage.php' );
@@ -50,20 +49,19 @@ require_once( PLUGIN_PATH . 'inc/mainpage.php' );
 /**
  * Zentrale Aktivierungsfunktion für das Plugin
  *
- * @since   1.0.0
+ * @since 1.0.0
  */
 
 function cmkk_plugin_activation()
 {
     global $wpdb;
 
-    /* Funktionsbibliothek einbinden */
 
+    // Funktionsbibliothek einbinden
     require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
 
 
-    /* Tabellen einrichten falls nicht vorhanden */
-
+    // Tabellen einrichten falls nicht vorhanden.
     $table_charset_collate = $wpdb->get_charset_collate();
     $table_name            = $wpdb->prefix . TABLE_POOL;
 
@@ -102,19 +100,17 @@ function cmkk_plugin_activation()
     endif;
 
 
-    /* Optionen einrichten falls nicht vorhanden */
-
-    if( FALSE == get_option( OPTION_MAIL_SUBJECT ) ) :
+    // Optionen einrichten falls nicht vorhanden
+    if( false == get_option( OPTION_MAIL_SUBJECT ) ) :
         add_option( OPTION_MAIL_SUBJECT, 'Vielen Dank für Ihre Teilnahme' );
     endif;
 
-    if( FALSE == get_option( OPTION_MAIL_MESSAGE ) ) :
+    if( false == get_option( OPTION_MAIL_MESSAGE ) ) :
         add_option( OPTION_MAIL_MESSAGE, 'Ihre Teilnahme am Interdisziplinären WundCongress wurde registriert. In den kommenden Tagen erhalten Sie eine weitere E-Mails mit zusätzlichen Informationen sowie den Zugangsdaten zur Veranstaltung.<br><br>Bis dahin alles Gute,<br>Ihr Team vom IWC.<br><br>Achtung: Diese Mail wurde automatisch generiert, bitte antworten Sie nicht hierauf.' );
     endif;
 
 
-    /* Pfad für Export-Dateien anlegen */
-
+    // Pfad für Export-Dateien anlegen
     $upload_dir = wp_upload_dir();
 
     wp_mkdir_p( $upload_dir['basedir'] . '/' . EXPORT_FOLDER );
@@ -127,7 +123,7 @@ register_activation_hook( __FILE__, 'cmkk_plugin_activation' );
 /**
  * Lädt Plugin-Scripts
  *
- * @since 0.0.1
+ * @since 1.0.0
  */
 
 function cmkk_admin_enqueue_scripts()
