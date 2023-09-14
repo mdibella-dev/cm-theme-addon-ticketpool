609df59ed5ae8960f71dbc4f1d561f6770c45f1b
Marco Di Bella <mdb@marcodibella.de>
2023-09-14 11:31:24 +0200

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -7,9 +7,9 @@
  * Author URI:          https://www.marcodibella.de
  * License:             MIT License
  * Requires at least:   5
- * Tested up to:        6.2
+ * Tested up to:        6.3
  * Requires PHP:        7
- * Version:             2.0.0
+ * Version:             2.0.1
  * Text Domain:         cm-theme-addon-ticketpool
  *
  *
@@ -28,7 +28,7 @@ defined( 'ABSPATH' ) or exit;
 
 /** Variables and definitions */
 
-define( __NAMESPACE__ . '\PLUGIN_VERSION', '2.0.0' );
+define( __NAMESPACE__ . '\PLUGIN_VERSION', '2.0.1' );
 define( __NAMESPACE__ . '\PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( __NAMESPACE__ . '\PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 
